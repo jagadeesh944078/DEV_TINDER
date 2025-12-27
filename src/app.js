adb58f5dedaad7148eac0e2cb8c8ee53e5113b99
jagadeesh944078 <vemulajagadeesh28@gmail.com>
2025-12-27 11:50:05 +0530

    updated readme file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ const express = require("express");
 //creating instance of express
 const app = express();
 
+// Case 1
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Request URL:", req.originalUrl);
+    res.send("User Info");
+    next();
+  },
+  (req, res) => {
+    res.send("User Info");
+  }
+);
 // app.use("/", (req, res) => {
 //   res.send("welcome to DEVTINDER");
 // });
