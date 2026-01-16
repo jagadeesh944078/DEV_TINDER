01a328823fb41cb7cc97708862e8b0c6428074a1
jagadeesh944078 <vemulajagadeesh28@gmail.com>
2026-01-16 19:44:23 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-const connectionrequest = require("../models/connectionRequest");
+const user = require("../models/user");
 const userRouter = express.Router();
 
 const USER_SAFE_FIELDS =
@@ -27,14 +27,12 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
-    const connectionRequests = await connectionrequest
-      .find({
-        $or: [
-          { fromUserId: loggedInUserId, status: "accepted" },
-          { toUserId: loggedInUserId, status: "accepted" },
-        ],
-      })
-      .populate("fromUserId toUserId", USER_SAFE_FIELDS);
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUserId, status: "accepted" },
+        { toUserId: loggedInUserId, status: "accepted" },
+      ],
+    }).populate("fromUserId toUserId", USER_SAFE_FIELDS);
     const data = connectionRequests.map((request) => {
       if (request.fromUserId.toString() === loggedInUserId.toString()) {
         return request.toUserId;
@@ -44,6 +42,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({
       message: "Connections fetched successfully",
       data: data,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUserId }, { toUserId: loggedInUserId }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((request) => {
+      hideUsersFromFeed.add(request.fromUserId.toString());
+      hideUsersFromFeed.add(request.toUserId.toString());
+    });
+
+    const users = await user
+      .find({
+        $and: [
+          { _id: { $nin: Array.from(hideUsersFromFeed) } },
+          { _id: { $ne: loggedInUserId } },
+        ],
+      })
+      .select(USER_SAFE_FIELDS)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({
+      message: "Feed fetched successfully",
+      data: users,
     });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
